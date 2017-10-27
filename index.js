ea4ca4b996a6bb3ef4c8d3ead63bc3b6c754443a
Diego Forero <gollum23@gmail.com>
2017-10-26 20:44:53 -0500

    [Update] Use ;

--- index.js
+++ index.js
@@ -4,57 +4,57 @@ const EMBED_REGEX = /@S\[([a-zA-Z].+)\]\([\s]*(.*?)[\s]*[\)]/im
 
 function podcast_embed(md, options) {
   function podcast_return(state, silent) {
-    var token
-    var serviceEnd
-    var serviceStart
-    var oldPos = state.pos
+    var token;
+    var serviceEnd;
+    var serviceStart;
+    var oldPos = state.pos;
 
     if (state.src.charCodeAt(oldPos) !== 0x40/* @ */ ||
       state.src.charCodeAt(oldPos + 1) !== 0x53/* S */) {
       return false;
     }
 
-    var match = EMBED_REGEX.exec(state.src)
+    var match = EMBED_REGEX.exec(state.src);
 
     if (!match || match.length < 3) {
       return false;
     }
 
     serviceStart = oldPos + 3;
-    serviceEnd = md.helpers.parseLinkLabel(state, oldPos + 1, false)
+    serviceEnd = md.helpers.parseLinkLabel(state, oldPos + 1, false);
 
     if (!silent) {
-      state.pos = serviceStart
-      state.posMax = serviceEnd
-      state.service = state.src.slice(serviceStart, serviceEnd)
-      var newState = new state.md.inline.State('soundcloud', state.md, state.env, [])
-      newState.md.inline.tokenize(newState)
+      state.pos = serviceStart;
+      state.posMax = serviceEnd;
+      state.service = state.src.slice(serviceStart, serviceEnd);
+      var newState = new state.md.inline.State('soundcloud', state.md, state.env, []);
+      newState.md.inline.tokenize(newState);
 
-      token = state.push('podcast', '')
-      token.podcastUrl = match[2]
-      token.service = 'soundcloud'
-      token.level = state.level
+      token = state.push('podcast', '');
+      token.podcastUrl = match[2];
+      token.service = 'soundcloud';
+      token.level = state.level;
     }
 
-    state.pos = state.pos + state.src.indexOf(')', state.pos)
+    state.pos = state.pos + state.src.indexOf(')', state.pos);
     state.posMax = state.tokens.length;
 
-    return true
+    return true;
   }
 
-  return podcast_return
+  return podcast_return;
 }
 
 
 function tokenize_podcast(md, options) {
   function tolenize_podcast_return(token, idx) {
-    var podcastUrl = md.utils.escapeHtml(token[idx].podcastUrl)
-    options.url = podcastUrl
+    var podcastUrl = md.utils.escapeHtml(token[idx].podcastUrl);
+    options.url = podcastUrl;
     return podcastUrl === '' ? '' :
-      '<div><iframe width="100%" height="' + options.height + '" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=' + options.url + '&amp;auto_play=' + options.auto_play + '&amp;hide_related=' + options.hide_related + '&amp;show_comments=' + options.show_comments + '&amp;show_user=' + options.show_user + '&amp;show_reposts=' + options.show_reposts + '&amp;visual=' + options.visual + '"></iframe></div>'
+      '<div><iframe width="100%" height="' + options.height + '" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=' + options.url + '&amp;auto_play=' + options.auto_play + '&amp;hide_related=' + options.hide_related + '&amp;show_comments=' + options.show_comments + '&amp;show_user=' + options.show_user + '&amp;show_reposts=' + options.show_reposts + '&amp;visual=' + options.visual + '"></iframe></div>';
   }
 
-  return tolenize_podcast_return
+  return tolenize_podcast_return;
 }
 
 var defaults = {
@@ -70,12 +70,12 @@ var defaults = {
 
 module.exports = function podcast_plugin(md, options) {
   if (typeof options === 'object') {
-    options = Object.assign({}, defaults, options)
+    options = Object.assign({}, defaults, options);
   }
   else {
-    options = defaults
+    options = defaults;
   }
 
-  md.renderer.rules.podcast = tokenize_podcast(md, options)
-  md.inline.ruler.before('emphasis', 'audio', podcast_embed(md, options))
+  md.renderer.rules.podcast = tokenize_podcast(md, options);
+  md.inline.ruler.before('emphasis', 'audio', podcast_embed(md, options));
 }
